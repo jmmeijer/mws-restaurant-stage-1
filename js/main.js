078106f59c0d057e68f477004af2f822b95220fc
Jesse Meijer <j.m.meijer@outlook.com>
2018-04-02 22:37:47 +0200

    fix: removed view details link
    Removed view detail link and placed link on image and header. As adviced on Lesson 13.8 and https://webaim.org/standards/wcag/checklist#sc2.4.9

--- js/main.js
+++ js/main.js
@@ -151,17 +151,25 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+    
+  const imageLink = document.createElement('a');
+  //link.innerHTML = restaurant.name;
+  imageLink.href = DBHelper.urlForRestaurant(restaurant);
+  li.append(imageLink);
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   image.alt = restaurant.name;
-  li.append(image);
-
+  imageLink.append(image);
+    
   const name = document.createElement('h1');
-  name.innerHTML = restaurant.name;
   name.tabIndex = 0;
   li.append(name);
+
+  const nameLink = document.createElement('a');
+  nameLink.innerHTML = restaurant.name;
+  name.append(nameLink);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
@@ -170,11 +178,6 @@ createRestaurantHTML = (restaurant) => {
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
   li.append(address);
-
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
 
   return li
 }
