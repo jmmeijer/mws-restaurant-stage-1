3b24ffd3a36311870485c175a440d1d8596e88f1
Jesse Meijer <j.m.meijer@outlook.com>
2018-07-28 16:12:55 +0200

    feat: Added MapBox API-key
    Added MapBox API-key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibXJmZWVsZ29vZCIsImEiOiJjams1aGh6OGUxampqM29yeWU2djh2YWpyIn0.qxPk11pu0nXl21HLOn_DFQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
