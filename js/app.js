85408bd57b98a1937bee18cfb68ec4f85392a93d
Jesse Meijer <j.m.meijer@outlook.com>
2018-08-15 15:32:43 +0200

    feat: Wrapped inside domcontentloaded eventlistener and converted to arrow functions
    Wrapped inside domcontentloaded eventlistener and converted to arrow functions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
+document.addEventListener('DOMContentLoaded', event => {
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js').then(function(registration) {
+  navigator.serviceWorker.register('./sw.js').then( registration => {
     console.log('Service worker registration succeeded:', registration);
-  }).catch(function(error) {
+  }).catch( error => {
     console.log('Service worker registration failed:', error);
   });
 } else {
   console.log('Service workers are not supported.');
 }
+});
